edda9e2c2f9aee72df28211291923dbc1d3e9829
Scott Danzig <sdanzig@gmail.com>
2012-11-25 21:52:40 -0500

    Initial commit

---
+++ Nov29/SDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SDAppDelegate.h
+//  Nov29
+//
+//  Created by Scott Danzig on 11/25/12.
+//  Copyright (c) 2012 Scott Danzig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nov29/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nov29
+//
+//  Created by Scott Danzig on 11/25/12.
+//  Copyright (c) 2012 Scott Danzig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
